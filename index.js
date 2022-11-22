555f80a178223ee5cb57d459bd65b8716a99de06
rajumb0232 <rajugowda0232@gmail.com>
2022-11-22 17:05:16 +0530

    added all menu project files

---
+++ index.js
@@ -0,0 +1,285 @@
+// hard coded menu items
+
+const menu = [
+    {
+        id: 1,
+        title:"Puliyogare",
+        category:"breakfast",
+        price: 40,
+        img:"https://i.ytimg.com/vi/85M7CPdm-wE/maxresdefault.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 2,
+        title:"Lemon rice",
+        category:"breakfast",
+        price: 35,
+        img:"https://www.feastingathome.com/wp-content/uploads/2020/08/Indian-lemon-rice_-14.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 3,
+        title:"Veg pulav",
+        category:"breakfast",
+        price: 40,
+        img:"https://www.ndtv.com/cooks/images/Vegetable-Pulao-620.jpg?downsize=200:155",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 4,
+        title:"Masala dosa",
+        category:"breakfast",
+        price: 50,
+        img:"https://t3.ftcdn.net/jpg/03/21/94/18/360_F_321941897_1IemuLDDsuBlNyK3nCy8DpHgW3DKJu2B.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 5,
+        title:"Rava idli",
+        category:"breakfast",
+        price: 40,
+        img:"https://southindianfoods.in/recipe_picture_enlarge/instant-oats-wheat-rava-idli.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 6,
+        title:"poori",
+        category:"breakfast",
+        price: 40,
+        img:"https://t4.ftcdn.net/jpg/01/15/65/19/360_F_115651965_lYL3HcRdknpnIDDtKhWzvsRamVDCCKun.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 7,
+        title:"Poha",
+        category:"breakfast",
+        price: 35,
+        img:"https://i.ytimg.com/vi/lIkp1YKV4Es/maxresdefault.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 8,
+        title:"South Indian",
+        category:"meals",
+        price: 120,
+        img:"https://www.lacademie.com/wp-content/uploads/2022/05/south-indian-foods.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 9,
+        title:"North Indian",
+        category:"meals",
+        price: 120,
+        img:"https://st2.depositphotos.com/5653638/11520/i/950/depositphotos_115207140-stock-photo-indian-thali-indian-food-thali.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 10,
+        title:"South mini",
+        category:"meals",
+        price: 70,
+        img:"https://media-cdn.tripadvisor.com/media/photo-s/0e/e5/1b/ba/mini-meals-thaali.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 11,
+        title:"North mini",
+        category:"meals",
+        price: 60,
+        img:"https://img.freepik.com/premium-photo/north-indian-mini-meal-parcel-platter-combo-thali-with-paneer-butter-masala-roti-dal-rice_466689-87326.jpg?w=2000",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 12,
+        title:"Unimited (any)",
+        category:"meals",
+        price: 250,
+        img:"https://theplanetd.com/images/popular-indian-dishes-cuisine.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 13,
+        title:"Samosa",
+        category:"snaks",
+        price: 20,
+        img:"https://wallpaperaccess.com/full/2069188.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 14,
+        title:"Paani poori",
+        category:"snaks",
+        price: 40,
+        img:"https://previews.123rf.com/images/indianfoodimages/indianfoodimages1805/indianfoodimages180500334/102976670-pani-puri-or-golgappa-is-a-popular-indian-chat-menu-selective-focus.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 15,
+        title:"Cabbage manchoorian",
+        category:"chinese",
+        price: 80,
+        img:"https://geekrobocook.com/wp-content/uploads/2021/04/Cabbage-manchurian.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 16,
+        title:"Mashroom Manchoorian",
+        category:"chinese",
+        price: 110,
+        img:"https://cookingfromheart.com/wp-content/uploads/2017/07/Mushroom-Manchurian-7.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 17,
+        title:"Bhel poori",
+        category:"snaks",
+        price: 40,
+        img:"https://media.istockphoto.com/id/1022824058/photo/bhelpuri-is-a-savoury-snack-or-chaat-it-is-made-out-of-puffed-rice-vegetables-and-a-tangy.jpg?s=612x612&w=0&k=20&c=-jywjAiAhE5hsiWLvCt_OylwRvCnl9sN40t6-PJpwoE=",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 18,
+        title:"Noodels",
+        category:"chinese",
+        price: 70,
+        img:"https://as1.ftcdn.net/v2/jpg/02/37/60/62/1000_F_237606251_djVLX0EZ9ruF5sTquwbviJOlng7L7I5b.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 19,
+        title:"Fried rice",
+        category:"chinese",
+        price: 90,
+        img:"https://kfoods.com/images1/newrecipeicon/chinese-egg-fried-rice_1418.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 20,
+        title:"Momos",
+        category:"chinese",
+        price: 70,
+        img:"https://b.zmtcdn.com/data/pictures/chains/5/19632505/77a9c81f7eb143b13dda1d0c32ffe1b3.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 21,
+        title:"Watermelon Juice",
+        category:"snaks",
+        price: 60,
+        img:"https://media.istockphoto.com/id/838270506/photo/summer-refreshing-detox-smoothie-of-watermelon-and-mint-on-a-white-wooden-background-rustic.jpg?s=612x612&w=0&k=20&c=e4q8qUVUZOc3gA5kwCk7iFFwoV9ET9_wwUF_HE4w7G8=",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 22,
+        title:"Muskmelon Juice",
+        category:"snaks",
+        price: 70,
+        img:"https://www.archanaskitchen.com/images/archanaskitchen/World_Beverages/Muskmelon_Kharbuj_Juice_Recipe-1.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 23,
+        title:"Apple Juice",
+        category:"snaks",
+        price: 100,
+        img:"https://thumbs.dreamstime.com/b/apple-juice-natural-fruits-garden-168362367.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 24,
+        title:"Grape Juice",
+        category:"snaks",
+        price: 100,
+        img:"https://www.waters.com/content/dam/waters/en/Photography/stock/food-and-environment/stock-grape-juice-in-glass.jpg",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 25,
+        title:"Filtered Coffee",
+        category:"snaks",
+        price: 20,
+        img:"https://img.freepik.com/premium-photo/south-indian-filter-coffee-served-traditional-brass-stainless-steel-cup_466689-83533.jpg?w=2000",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+
+    {
+        id: 26,
+        title:"Tea",
+        category:"snaks",
+        price: 20,
+        img:"https://media.istockphoto.com/id/864607392/photo/image-of-a-glass-of-tea-in-street-market.jpg?s=170667a&w=0&k=20&c=tB55jlq3zwOJWZepbh-FzlADsSgiYCy-LJqUEHoQlsk=",
+        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius adipisci doloribus natus aperiam ut placeat numquam enim, eaque aut quos?"
+    },
+]
+
+
+const sectionCenter = document.querySelector(".section-center");
+const filterbtns = document.querySelectorAll(".filter-btn");
+
+//to render all the menu items ----
+window.addEventListener('DOMContentLoaded',function(){
+    displayMenuItems(menu);
+})
+
+// to filter and render the menu items ----
+filterbtns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+        const category = e.currentTarget.dataset.id ;
+        const filteredMenu= menu.filter(function(menuItems){
+            if(menuItems.category === category){
+                return menuItems
+            }
+        })
+        if(category === 'all'){
+            displayMenuItems(menu);
+        }
+        else{
+            displayMenuItems(filteredMenu);
+        }
+    })
+})
+
+
+// to display filtered items ----
+const displayMenuItems =(menuItems) =>{
+    let displayMenu = menuItems.map(function(item){
+        
+        return `<article class="menu-item">
+        <img src=${item.img} alt="">
+        <div class="item-info">
+            <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}/-</h4>
+            </header>
+            <p class="item-data">${item.desc}</p>
+        </div>
+    </article>`
+    })
+    displayMenu = displayMenu.join("")
+    sectionCenter.innerHTML = displayMenu
+}
+
